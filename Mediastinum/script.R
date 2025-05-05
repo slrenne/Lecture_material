6ac24806d5ba3b969dcfc25b08ec94e9e0c52611
Salvatore Lorenzo Renne <salvatore.renne@hunimed.eu>
2025-05-05 23:27:59 +0200

    mediastinum added

---
+++ Mediastinum/script.R
@@ -0,0 +1,110 @@
+library(tidyverse)
+library(ggdark)
+
+db <- read.csv("Mediastinum/input/RodenSupp4.csv")
+
+db %>% ggplot(mapping = aes(
+  x = reorder(diagnosis, total),
+  y = total)) + 
+  geom_col() +
+  coord_flip()+ 
+  labs( title = 'Mediastinal Neoplasm',
+        x = 'Histotype', 
+        y = 'Number of cases')+
+  dark_theme_gray()
+
+ggsave('Mediastinum/output/total.png', 
+       width = 16,
+       height = 9,
+       dpi = 300)
+
+
+db %>% gather('Prevascular', 'Visceral', 'Para.vertebral',
+              key = 'compartment', 
+              value = 'cases') %>% 
+  ggplot(mapping = aes(
+    x = reorder(diagnosis, cases),
+    y = cases)) + 
+  geom_col() +
+  coord_flip()+ 
+  labs( title = 'Mediastinal Neoplasm',
+        x = 'Histotype', 
+        y = 'Number of cases')+
+  facet_grid(.~ compartment)+
+  dark_theme_gray()
+  
+
+ggsave('Mediastinum/output/total_facet.png', 
+       width = 16,
+       height = 9,
+       dpi = 300)
+
+tot <- sum(db$total)
+db %>% gather('Prevascular', 'Visceral', 'Para.vertebral',
+              key = 'compartment', 
+              value = 'cases') %>% 
+  filter(compartment == 'Prevascular') %>%
+  arrange(desc('cases')) %>% head(10) %>%
+  ggplot(mapping = aes(
+    x = reorder(diagnosis, cases),
+    y = cases/tot, 
+    fill = 'magenta')) + 
+  geom_col() +
+  coord_flip()+ 
+  labs( title = 'Prevascular',
+        x = 'Histotype', 
+        y = 'Proportion of cases')+
+  guides(fill='none')+
+  dark_theme_gray()
+  
+
+ggsave('Mediastinum/output/topPrevascular.png', 
+       width = 5,
+       height = 9,
+       dpi = 300)
+
+db %>% gather('Prevascular', 'Visceral', 'Para.vertebral',
+              key = 'compartment', 
+              value = 'cases') %>% 
+  filter(compartment == 'Visceral') %>%
+  arrange(desc('cases')) %>% head(10) %>%
+  ggplot(mapping = aes(
+    x = reorder(diagnosis, cases),
+    y = cases/tot, 
+    fill = 'dodgerblue')) + 
+  geom_col() +
+  coord_flip()+ 
+  labs( title = 'Visceral',
+        x = 'Histotype', 
+        y = 'Proportion of cases')+
+  guides(fill='none')+
+  dark_theme_gray()
+
+
+ggsave('Mediastinum/output/topVisceral.png', 
+       width = 5,
+       height = 9,
+       dpi = 300)
+
+db %>% gather('Prevascular', 'Visceral', 'Para.vertebral',
+              key = 'compartment', 
+              value = 'cases') %>% 
+  filter(compartment == 'Para.vertebral') %>%
+  arrange(desc('cases')) %>% head(10) %>%
+  ggplot(mapping = aes(
+    x = reorder(diagnosis, cases),
+    y = cases/tot, 
+    fill = 'yellow')) + 
+  geom_col() +
+  coord_flip()+ 
+  labs( title = 'Paravertebral',
+        x = 'Histotype', 
+        y = 'Proportion of cases')+
+  guides(fill='none')+
+  dark_theme_gray()
+
+
+ggsave('Mediastinum/output/topParavertebral.png', 
+       width = 5,
+       height = 9,
+       dpi = 300)
